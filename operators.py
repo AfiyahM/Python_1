458d29c909c6a0c4427213aa410827d3953b301b
AfiyahM <136073607+AfiyahM@users.noreply.github.com>
2024-07-22 15:42:19 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,84 @@
+x=10
+y=20
+print("ARITHMATIC OPERATORS")
+print(x,y)
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(x//y)
+print(x%y)
+print("")
+
+print("ASSIGNMENT OPERATORS")
+x+=6
+print(x,"Addition")
+x-=6
+print(x,"Subtraction")
+x*=6
+print(x,"Multipilcation")
+x/=6
+print(x,"division")
+x%=6
+print(x,"modules")
+y&=6
+print(y,"And operation")
+y|=6
+print(y,"OR Operation")
+y^=6
+print(y,"XOR Operation")
+y>>=2
+print(y,"left shift")
+y<<=2
+print(y, "Right Shift")
+
+print(g:=3)
+print("")
+
+print("COMPARISION OPERATORS")
+print("x==y",x==y)
+print("x>=y",x>=y)
+print("x<=y",x<=y)
+print("x!=y",x!=y)
+print("x>y",x>y)
+print("x<y",x<y)
+print("")
+
+print("LOGICAL OPERATORS")
+a=10 
+b=50
+print(a,b)
+print( "Logical and",a>1 and b>49)
+print( "Logical or", a>45 or b<49)
+#print("Logical not", not a=10)
+print("")
+
+print("IDENTITY OPERATOR")
+print(a is b,"is operator")
+print(a is not b,"is not operator")
+
+print("MEMBERSHIP OPERATOR")
+p=["Afiyah", "Gayatri"]
+print(p)
+print("Afiyah" in p,"is operator")
+print("Afiyah" not in p,"is not operator")
+print("")
+
+print("BITEWISE OPERATOR")
+m=10
+n=20
+print(m&n)
+print(m|n)
+print(m^n)
+print(~m)
+print(m>>n)
+print(m<<n)
+
+
+
+
+
+
+
