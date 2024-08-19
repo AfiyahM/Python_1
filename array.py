da87178beda98cafb5f83ce55cde72280004ea48
AfiyahM <136073607+AfiyahM@users.noreply.github.com>
2024-08-19 15:45:13 +0530

    string methods

--- array.py
+++ array.py
@@ -1,11 +1,20 @@
 a= ["Afiyah"]
 print(a.append("Modak"))
-print(a.copy())
-print(a.count([0]))
-print(a.insert(2,"Asif"))
 print(a.index("Afiyah"))
+print(a)
+a.reverse()
+print(a)
+a.insert(1,"Asif")
+print(a)
+a.pop(1)
+print (a)
 
-print(a.reverse())
+
+b=[1,5,3,2,7,12,12,36,34,465,64,0]
+print(b)
+b.sort()
+print(b)
+
 
 
 
